43dd6dc00efb837b2b712e69b4bad3bac4d5ea16
Yeakub1 <mryeakub10@gmail.com>
2023-04-12 11:27:57 +0600

    Job Details Done

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import {
+  CurrencyDollarIcon,
+  CalendarDaysIcon,
+  PhoneIcon,
+  InboxStackIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/solid";
 
 const Details = ({ data }) => {
-  const { description, responsibility, edu, exp, edue, expe, salary, title } =
-    data;
+  const {
+    description,
+    responsibility,
+    edu,
+    exp,
+    salary,
+    title,
+    Phone,
+    Email,
+    Address,
+  } = data;
   return (
     <div className="px-20 ">
       <h1 className="mt-10 text-center bottom-0 relative font-bold text-4xl bg-slate-200 p-10">
@@ -30,15 +46,46 @@ const Details = ({ data }) => {
           </h1>
           <hr />
           <div className=" p-7">
-            <h2>
-              <span className="font-bold text-lg">Salary:</span> {salary}
-            </h2>
-            <h2>
-              <span className="font-bold text-lg">Job Title: </span>
-              {title}
-            </h2>
+            <div className="flex">
+              <CurrencyDollarIcon className="h-6 w-6 text-black" />
+              <h2 className="mb-5">
+                <span className="font-bold text-lg"> Salary:</span> {salary}
+              </h2>
+            </div>
+
+            <div className="flex ">
+              <CalendarDaysIcon className="h-6 w-6 text-black" />
+              <h2>
+                <span className="font-bold text-lg">Job Title: </span>
+                {title}
+              </h2>
+            </div>
+
             <h1 className="font-bold text-xl mt-5 mb-5">Contact Information</h1>
             <hr />
+
+            <div className="flex ">
+              <PhoneIcon className="h-6 w-6 text-black" />
+              <h2 className="mb-5">
+                <span className="font-bold text-lg ">Phone: </span> {Phone}
+              </h2>
+            </div>
+
+            <div className="flex ">
+              <InboxStackIcon className="h-6 w-6 text-black" />
+              <h2 className="mb-5">
+                <span className="font-bold text-lg ">Email: </span> {Email}
+              </h2>
+            </div>
+            <div className="flex ">
+              <MapPinIcon className="h-6 w-6 text-black" />
+              <h2>
+                <span className="font-bold text-lg">Address: </span> {Address}
+              </h2>
+            </div>
+          </div>
+          <div className="text-center px-4 py-2 bg-blue-400 text-lg font-semibold">
+            <button className="">Apply Now</button>
           </div>
         </div>
       </div>
