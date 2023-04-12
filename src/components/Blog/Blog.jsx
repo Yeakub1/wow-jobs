232ae8d0e3a16b833ed3c026df06c12b17cfcc44
Yeakub1 <mryeakub10@gmail.com>
2023-04-12 19:47:34 +0600

    contex api

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div className='mt-20'>
-            <h1>Blog for this is a title in the our sites</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem commodi consequuntur pariatur, temporibus, suscipit animi delectus asperiores aut aliquid soluta dolorum excepturi? Voluptate voluptatem quaerat laudantium mollitia modi eius rem.</p>
-        </div>
+      <div className="m-20 px-20">
+        <h1>
+          <span className="font-semibold text-xl">Context API:</span> The
+          Context API is a feature in React that allows you to manage the global
+          state and share data between components without the need to pass props
+          through every level of the component tree. It provides a way to pass
+          data down to child components without having to pass props explicitly
+          at every level, making it easier to manage and share state across
+          different parts of your application.
+        </h1>
+        
+      </div>
     );
 };
 
