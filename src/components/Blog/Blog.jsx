9477aaadfa4a8d8e12b378cc674688492aec11c2
Yeakub1 <mryeakub10@gmail.com>
2023-04-12 19:48:27 +0600

    custom hook

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -12,6 +12,14 @@ const Blog = () => {
           at every level, making it easier to manage and share state across
           different parts of your application.
         </h1>
+        <h1 className="mt-5">
+          <span className="font-semibold text-xl">custom hook:</span> custom
+          hook is a special JavaScript function whose name starts with 'use' and
+          can be used to call other hooks. A custom hook does not require a
+          specific signature. A software developer can choose what argument the
+          custom hook has and what should the argument return. A custom hook
+          always starts with the name
+        </h1>
         
       </div>
     );
